55fc48eaf2fd1d4f87e4f4543788f66f7b2aee05
Sail338 <srihari.shankar1@gmail.com>
2018-04-21 14:13:55 -0400

    pushed

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import requests
+from clarifai.rest import ClarifaiApp
 import config
 import webbrowser
 
 def translate(data:str):
     a = requests.get("https://api.giphy.com/v1/gifs/translate?api_key=" +config.API_KEY + "&s="+data) 
     dat = a.json()
-    print(dat['data']['url'])
+ #   print(dat['data']['url'])
     webbrowser.open_new(dat['data']['images']['fixed_height']['url'])
 translate("Rutgers")
 
 def random():
     a = requests.get("https://api.giphy.com/v1/gifs/random?api_key=" +config.API_KEY) 
     dat = a.json()
-    print(dat['data']['url'])
+#    print(dat['data'])
     webbrowser.open_new(dat['data']['images']['fixed_height']['url'])
 random()
+
+
+def clarifai_test(url_:str):
+    app = ClarifaiApp(api_key=config.CLARFAI)
+    model = app.models.get("general-v1.3")
+    b =model.predict_by_url(url=url_)
+    for k in b['outputs']:
+        for i in k['data']['concepts']:
+            print(i['name'])
+
+#clarifai_test("https://samples.clarifai.com/metro-north.jpg")
 
